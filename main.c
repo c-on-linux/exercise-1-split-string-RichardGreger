e55b392f1975b163c42e960d2e4e3d421ff869b9
RichardGreger <42612150+RichardGreger@users.noreply.github.com>
2019-10-17 12:56:36 +0200

    First update for assignment one
    Added comments where I have questions.

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 struct Token {
-  const char *token;
-  size_t token_length; // Number of characters
+    const char *token;
+    size_t token_length; // Number of characters
 };
 
 /**
@@ -14,8 +15,18 @@ struct Token {
  * @return the number of tokens in input
  */
 int count_tokens(const char *input) {
-  (void) input;
-  return 0;
+    if (input == NULL || strlen(input) == 0)
+        return NULL;
+    int numTokens = 0;
+    char* localCopy = malloc(strlen(input) + 1);
+    strncpy(localCopy, input, strlen(input) + 1);
+    char *currentToken;
+    do {
+        currentToken = strsep(&localCopy, " ");
+        if (*currentToken != NULL && *currentToken != '\n')
+            ++numTokens;
+    } while (localCopy != NULL);
+    return numTokens;
 }
 
 /**
@@ -26,10 +37,23 @@ int count_tokens(const char *input) {
  * @return the number of tokens in input
  */
 int split_into_strings(const char *input, char ***output) {
-  (void) input;
-  (void) output;
-  return 0;
+    if (input == NULL || strlen(input) == 0) {
+        return 0;
+    }
+    int numTokens = 0;
+
+    char* localCopy = malloc(strlen(input) + 1);
+    strncpy(localCopy, input, strlen(input) + 1);
+    
+    // Why would we malloc this amount of space? 
+    // I must have writting something wrong during the class
+    *output = malloc(count_tokens(input)*sizeof(char));
+    
+    // Todo: Finish from here
+
+    return numTokens;
 }
+
 
 /**
  * Splits the input string into Token structs
@@ -38,45 +62,47 @@ int split_into_strings(const char *input, char ***output) {
  * @return the number of tokens in input
  */
 int split_into_tokens(const char *input, struct Token **output) {
-  (void) input;
-  (void) output;
-  return 0;
+    (void) input;
+    (void) output;
+    return 0;
 }
 
 void testCountTokens() {
-  assert(0 == count_tokens(""));
-  assert(0 == count_tokens(0));
-  assert(4 == count_tokens("This is a string"));
-  assert(1 == count_tokens(" One "));
-  assert(2 == count_tokens("One  Two "));
-  assert(6 == count_tokens("This is an  even longer  string "));
+    assert(0 == count_tokens(""));
+    assert(0 == count_tokens(0));
+    assert(4 == count_tokens("This is a string"));
+    assert(1 == count_tokens(" One "));
+    assert(2 == count_tokens("One  Two "));
+    assert(6 == count_tokens("This is an  even longer  string "));
 }
 
 void testMakeTokenStrings() {
-  char **tokens = 0;
-  assert(4 == split_into_strings("This is a string", &tokens));
-  assert(0 == strcmp(tokens[0], "This"));
-  assert(0 == strcmp(tokens[1], "is"));
-  assert(0 == strcmp(tokens[2], "a"));
-  assert(0 == strcmp(tokens[3], "string"));
+    // Dette er et array av ord, altså array av array av chars
+    char **tokens = 0;
+    
+    assert(4 == split_into_strings("This is a string", &tokens));
+    assert(0 == strcmp(tokens[0], "This"));
+    assert(0 == strcmp(tokens[1], "is"));
+    assert(0 == strcmp(tokens[2], "a"));
+    assert(0 == strcmp(tokens[3], "string"));
 }
 
 void testMakeTokenStructs() {
-  struct Token *tokens = 0;
-  assert(4 == split_into_tokens("This is a string", &tokens));
-  assert(tokens[0].token_length == 4);
-  assert(0 == strncmp(tokens[0].token, "This", 4));
-  assert(tokens[1].token_length == 2);
-  assert(0 == strncmp(tokens[1].token, "is", 2));
-  assert(tokens[2].token_length == 1);
-  assert(0 == strncmp(tokens[2].token, "a", 1));
-  assert(tokens[3].token_length == 6);
-  assert(0 == strncmp(tokens[3].token, "string", 6));
+    struct Token *tokens = 0;
+    assert(4 == split_into_tokens("This is a string", &tokens));
+    assert(tokens[0].token_length == 4);
+    assert(0 == strncmp(tokens[0].token, "This", 4));
+    assert(tokens[1].token_length == 2);
+    assert(0 == strncmp(tokens[1].token, "is", 2));
+    assert(tokens[2].token_length == 1);
+    assert(0 == strncmp(tokens[2].token, "a", 1));
+    assert(tokens[3].token_length == 6);
+    assert(0 == strncmp(tokens[3].token, "string", 6));
 }
 
 int main() {
-  testCountTokens();
-  testMakeTokenStrings();
-  testMakeTokenStructs();
-  return EXIT_SUCCESS;
+    testCountTokens();
+    testMakeTokenStrings();
+    testMakeTokenStructs();
+    return EXIT_SUCCESS;
 }
